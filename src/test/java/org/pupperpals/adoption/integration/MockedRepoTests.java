8654a163cc4f21cb600dd0bd76cb8db2ed4b3070
Michael Gaffney <michaelgaffney@gMichaelGaffney.local>
2021-07-01 15:51:47 -0500

    First RestDoc

--- src/test/java/org/pupperpals/adoption/integration/MockedRepoTests.java
+++ src/test/java/org/pupperpals/adoption/integration/MockedRepoTests.java
@@ -5,6 +5,7 @@ import org.pupperpals.adoption.model.Pupper;
 import org.pupperpals.adoption.repository.PupperRepository;
 import org.pupperpals.adoption.service.PupperService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -16,12 +17,16 @@ import java.util.ArrayList;
 import java.util.List;
 
 import static org.mockito.Mockito.when;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@AutoConfigureRestDocs(outputDir = "src/docs")
 public class MockedRepoTests {
     @Autowired
     private MockMvc mvc;
@@ -42,7 +47,12 @@ public class MockedRepoTests {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("length()").value(2))
                 .andExpect(jsonPath("[0]").value(puppers.get(0)))
-                .andExpect(jsonPath("[1]").value(puppers.get(1)));
+                .andExpect(jsonPath("[1]").value(puppers.get(1)))
+                .andDo(document("puppers GET", responseFields(
+                        fieldWithPath("id").description("Internal ID for use with PATCH"),
+                        fieldWithPath("name").description("The name of the pupper"),
+                        fieldWithPath("breed").description("The name of the breed of the pupper"))
+                ));
     }
 
 }
